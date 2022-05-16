32224d764aca1a2ef9aad83ca98adbc35443f1ac
serhatuluc <99399280+serhatuluc@users.noreply.github.com>
2022-05-16 23:46:50 +0300

    Update Item.py

--- Item.py
+++ Item.py
@@ -11,25 +11,19 @@ class Item:
         assert quantity >= 0, f"Price {quantity} is not greater than zero!"
 
         # Assign to self object
-        self.__name = name #Single underscore would work but it makes itself reachable
+        self.__name = name 
         self.__price = price
         self.quantity = quantity
 
         # Actions to execute
-        Item.all.append(self)
-
-        # ---------------------------------------------------
-        # Training
-        # print(f"An instance created: {name}")
-        # print("I AM CREATED")   #please check it to understand fully
-        # -----------------------------------------------------
+        Item.all.append(self)  
 
     @property
     def price(self):
         return self.__price
 
     def apply_discount(self):
-        self.__price = self.__price * self.pay_rate  # Item.pay_rate can be used. Check it to grab it
+        self.__price = self.__price * self.pay_rate  
 
     def apply_increment(self,increment_value):
         self.__price=self.__price+self.__price*increment_value
